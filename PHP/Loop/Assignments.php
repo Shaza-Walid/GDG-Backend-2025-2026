f42d35b856cbb16b453847dfa574accd5d9be06f
Shaza-Walid <shazawalid2025@gmail.com>
2026-03-23 00:23:51 +0200

    .

--- PHP/Loop/Assignments.php
+++ PHP/Loop/Assignments.php
@@ -33,4 +33,63 @@ while ($num < 520) {
 }
 
 #assignment 4:
+$start = 10;
+$end = 0;
+$stop = 3;
+for($i=$start; $i!=$stop-1; $i--){
+    if($i<10)
+        echo $end.$i."<br>";
+    else
+        echo $i."<br>";
+}
 
+#assignment 5:
+$start = 0;
+$mix = [1, 2, 3, "A", "B", "C", 4];
+for ($i = $start; $i < count($mix); $i++) {
+    //ignoring the first element
+    if ($i == $start) 
+        continue;
+    // echo numbers only
+    if (is_numeric($mix[$i]))
+        echo $mix[$i] . "<br>";
+}
+
+#assignment 6:
+$money = ["Ahmed" => 100, "Sayed" => 150, "Osama" => 100, "Maher" => 250];
+foreach ($money as $name => $amount)
+    echo "The Name Is $name And I Need $amount Pound From Him<br>";
+
+#assignment 7:
+$mix = [1, 2, "A", "B", "C", 3, 4];
+$numbers = 0;
+$letters = 0;
+foreach($mix as $item) {
+    if(is_numeric($item)){
+        echo $item . "<br>";
+        $numbers++;
+    }else
+        $letters++;
+}
+echo "\"$numbers Numbers Printed\"<br>";
+echo "\"$letters Letters Ignored\"<br>";
+
+#assignment 8:
+$nums = [1, 13, 12, 20, 51, 17, 30];
+foreach($nums as $num)
+    if($num % 2 == 0)
+        echo $num / 2 . "<br>";
+
+#assignment 9:
+$help_num = 3;
+$nums = [4, 5, 6, 1, 2, 3];
+$names = ["Ahmed", "Sayed", "Osama", "Mahmoud", "Gamal"];
+for ($i = $help_num - 2; $i < $help_num; $i++)
+    echo "\"$names[$i]\"<br>";
+
+#assignment 10:
+$help_num = 4;
+$nums = [2, 4, 5, 6, 10];
+foreach ($nums as $index => $num)
+    echo "\"$num + " . $nums[count($nums) - $index - ($help_num / $help_num)] . " = " 
+    . ($num + $nums[count($nums) - $index - ($help_num / $help_num)]) . "\"<br>";
